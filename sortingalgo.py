7561f8f85b4664cd742a4c088a633fa6110b1c3e
nirmit1011 <nirmitkhurana24@gmail.com>
2021-12-27 19:19:52 +0530

    correct dropdown

--- sortingalgo.py
+++ sortingalgo.py
@@ -7,12 +7,13 @@ root.title("Sorting Algorithm Visualiser")
 root.geometry('900x600+200+80')
 root.config(bg = "#082A46")
 
+selected_algorithm = StringVar()
 # label, buttons, speed scale
 
 mainlabel = Label(root, text = "Algorithm : ", font = ("new roman", 16,"italic bold"),bg = "#05897A",width = 10, fg = 'black', relief = GROOVE, bd = 5)
 mainlabel.place(x=0,y=0)
 
-algo_menu = ttk.Combobox(root, width=15, font = ("new roman", 19,'itaclic bold'), textvariable = selected_algorithm, values=['Bubble Sort','Merge Sort','Quick Sort'])
+algo_menu = ttk.Combobox(root, width=16, font = ("new roman", 19,'italic bold'), textvariable = selected_algorithm, values=['Bubble Sort','Merge Sort','Quick Sort'])
 algo_menu.place(x=145, y=0)
 algo_menu.current(0)
 root.mainloop()
