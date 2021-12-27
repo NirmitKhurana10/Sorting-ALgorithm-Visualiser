74c1a1dca1b08b4f8448142b6173819f87802bab
nirmit1011 <nirmitkhurana24@gmail.com>
2021-12-27 20:02:35 +0530

    Creating Generate button to call function generate

--- sortingalgo.py
+++ sortingalgo.py
@@ -7,6 +7,9 @@ root.title("Sorting Algorithm Visualiser")
 root.geometry('900x600+200+80')
 root.config(bg = "#082A46")
 
+def Generate():
+    print("Selected Algorithm: ")
+
 selected_algorithm = StringVar()
 # label, buttons, speed scale
 
@@ -15,5 +18,12 @@ mainlabel.place(x=0,y=0)
 
 algo_menu = ttk.Combobox(root, width=16, font = ("new roman", 19,'italic bold'), textvariable = selected_algorithm, values=['Bubble Sort','Merge Sort','Quick Sort'])
 algo_menu.place(x=145, y=0)
-algo_menu.current(0)
+algo_menu.current(0) # by default bubble sort
+
+random_generate = Button(root, text = "Generate", bg = '#2DAE9A', font = ("arial", 12, 'italic bold'), relief = SUNKEN, activebackground = "#05945B", activeforeground = "white", bd = 5, width = 10, command = Generate) 
+random_generate.place(x = 750, y = 60)
+
+sizevaluelable = Label(root, text = "Size : ", font = ("new roman",12,"italic bold"), bg = "#0E6DA5",width = 10, fg = "black", height = 2, relief = GROOVE, bd = 5)
+sizevaluelable.place(x = 0, y = 60)
+
 root.mainloop()
