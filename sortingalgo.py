5f7a7083bf2f615912c77c09613b044eb3c84163
nirmit1011 <nirmitkhurana24@gmail.com>
2021-12-28 05:36:39 +0530

    Quick sort complete with color

--- sortingalgo.py
+++ sortingalgo.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import random
 from bubblesort import bubble_sort
-
+from quicksort import quick_sort
 root = Tk()
 root.title("Sorting Algorithm Visualiser")
 root.geometry('900x600+200+80')
@@ -32,7 +32,14 @@ def drawData(data, colorArray):
 
 def StartAlgorithm():
     global data
-    bubble_sort(data, drawData,speedscale.get())
+    if not data:
+        return 
+    
+    if(algo_menu.get() == "Bubble Sort"):
+        bubble_sort(data, drawData,speedscale.get())
+    elif(algo_menu.get() == "Quick Sort"):
+        quick_sort(data,0, len(data)-1, drawData,speedscale.get())
+        drawData(data, ['green' for x in range(len(data))])
 
 
 def Generate():
